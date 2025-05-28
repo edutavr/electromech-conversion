031e357de4c82420c075540a26370110a8f82d6e
Maria Silva <maria.rodrigues@tempest.com.br>
2025-05-28 18:10:10 -0300

    Finishing desafio_1

--- Desafio_1_e_2/desafio_1.py
+++ Desafio_1_e_2/desafio_1.py
@@ -11,7 +11,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 def initialize_parameters(W2, V2, V1):
     
     if W2.isdigit() & V2.isdigit() & V1.isdigit():
-        if W2 > 800:
+        if int(W2) > 800:
             print('Potência de saída do transformador é maior do que 800 VA. Não é possível usar lâminas padronizadas')   
             laminas_padrozinadas = False
         else:
@@ -32,7 +32,7 @@ def first_and_second_current(W2,V2,V1):
     return Is,Ip
 
 def conductor_section(current,W2):
-    if W2 == 500:
+    if W2 <= 500:
         return current/3
     elif W2 > 500 & W2 <= 1000:
         return  current/2.5
@@ -57,8 +57,7 @@ def bitola(condutor_section):
         return []
 
 
-def magnectic_section(potency, frequency,is_long_cable,
-                      is_two_primary_circuits=False,is_two_secondary_circuits=False):
+def magnectic_section(potency, frequency,is_long_cable,is_two_primary_circuits=False,is_two_secondary_circuits=False):
     standard_cables = 7.5*(math.sqrt(potency/frequency))
     long_cables = 6.5*(math.sqrt(potency/frequency))
     if is_two_primary_circuits is False and is_two_secondary_circuits is False:
@@ -90,11 +89,7 @@ def core_geometric_section_1(magnetic_section):
     return magnetic_section*1.1
 
 
-def calculate_a_and_b_geometric_section(geometric_section):
-    if geometric_section > 25:
-        a = 5
-    else:
-        a = 4
+def calculate_a_and_b_geometric_section(geometric_section,a):
     b = geometric_section/a
     return b
 
@@ -107,31 +102,31 @@ def core_magnetic_section(length, width):
 def calculate_turns_number_1(frequency, tension, core_ms):
     if(frequency == 50):
         return tension*(40/core_ms)
+    #Frequency = 60Hz
     return tension*(33.5/core_ms)
 
 
-def iron_core_weight(geometric_section):
-    b = calculate_a_and_b_geometric_section(geometric_section)
+def blades_qtd(b:float,acesita:float):
+  return round((b*0.9)/acesita)
 
-    if(geometric_section>25):
-        return 1.580*b
-    return 1.000*b
-
-
-def cooper_section(n1,s1,n2,s2):
-    return n1*s1 + n2*s2
-
-
-def calculate_lm(core_gs):
-    b = calculate_a_and_b_geometric_section(core_gs)
-
-    if(core_gs>25):
-        return (2*5 + 2*b + 0.5*3.14*5)
-    return (2*4 + 2*b + 0.5*3.14*4)
-
-def cooper_weight(section_cooper, lm):
-    return ((section_cooper/100)*lm*9)*0.001
-
+def blade_type(a): 
+    if a == 1.5: 
+        return "Lãmina tipo 0"
+    elif a == 2:
+        return "Lãmina tipo 1"
+    elif a == 2.5: 
+        return "Lãmina tipo 2"
+    elif a == 3: 
+        return "Lãmina tipo 3"
+    elif a == 3.5: 
+        return "Lãmina tipo 4"
+    elif a == 4: 
+        return "Lãmina tipo 5"
+    elif a == 5:
+        return "Lâmina tipo 6"
+    else: 
+        return "Lãmina não encontrada"
+    
 
 def dimensions_core(a,b,second_potency):
     if(second_potency>800):
@@ -140,13 +135,123 @@ def dimensions_core(a,b,second_potency):
         comprimento=b
         seção_janela=(0.5*a*1.5*a)*100  ## mm²
         volume= ((largura*altura) - (0.5*a*3*a*2))*b*0.9 ## cm³
+        peso = 5.4*(a**2)*7.8
 
 
         return ("\nA Largura é: "+str(largura)+"cm\n" +
                 "A Altura é: "+str(altura)+"cm\n" +
                 "O Comprimento é: "+str(comprimento)+"cm\n" +
                 "A Seção da janela é: "+str(seção_janela)+"mm²\n"+
-                "O Volume é: "+str(volume)+"cm³")
+                "O Volume é: "+str(volume)+"cm³\n"
+                "Peso é:" + str(peso)+ 'g')
+    else:
+        largura=3*a
+        altura=2.5*a
+        print("a", a)
+        print("largura ", largura)
+        print("altura ", altura)
+        comprimento=b
+        seção_janela=(0.5*a*1.5*a)*100  ## mm²
+        volume= ((float(largura)*float(altura)) - (0.5*a*3*a*2))*b*0.9 ## cm
+        peso = 5.4*(a**2)*7.8
+
+
+        return ("\nA Largura é: "+str(largura)+"cm\n" +
+                "A Altura é: "+str(altura)+"cm\n" +
+                "O Comprimento é: "+str(comprimento)+"cm\n" +
+                "A Seção da janela é: "+str(seção_janela)+"mm²\n"+
+                "O Volume é: "+str(volume)+"cm³\n"
+                "Peso é:" + str(peso)+ 'g')
+
+
+def create_transformer_sections(x, y, z, dx, dy, dz):
+    return np.array([[x, y, z],
+                     [x + dx, y, z],
+                     [x + dx, y + dy, z],
+                     [x, y + dy, z],
+                     [x, y, z + dz],
+                     [x + dx, y, z + dz],
+                     [x + dx, y + dy, z + dz],
+                     [x, y + dy, z + dz]])
+
+
+def rotate_transformer(vertices, angle):
+    rotation_matrix = np.array([
+        [ np.cos(angle), 1,0],
+        [0,-np.cos(angle),1],
+        [ np.sin(angle), np.cos(angle),0]
+    ])
+    return np.dot(vertices, rotation_matrix.T)
+
+
+def plot_transformer(ax, vertices, c):
+    faces = [[vertices[j] for j in [0, 1, 5, 4]],
+             [vertices[j] for j in [7, 6, 2, 3]],
+             [vertices[j] for j in [0, 3, 7, 4]],
+             [vertices[j] for j in [1, 2, 6, 5]],
+             [vertices[j] for j in [0, 1, 2, 3]],
+             [vertices[j] for j in [4, 5, 6, 7]]]
+
+    poly3d = Poly3DCollection(faces, linewidths=1, alpha=.35, color=c)
+    ax.add_collection3d(poly3d)
+
+
+def generate_transformer(ax, angle:float, a:float, b:float, first_tension:float, second_tension:float):
+
+    parts = [                     # X, Y, Z, H, W, T
+        create_transformer_sections(0, 0, 0, 0.5*a, 3*a, b),  # base da lamina "E"
+        create_transformer_sections(0, a+1.5*a, 0, 2*a, a*0.5, b),  # seção do secundario
+        create_transformer_sections(0, a, 0, 2*a, a, b),  # tronco central do transformador
+        create_transformer_sections(0, 0, 0, 2*a, a*0.5, b),  # seção do primario
+        create_transformer_sections(2*a, 0, 0, 0.5*a, 3*a, b)   # Seção que compõe a lamina tipo "I" (topo do transformador)
+    ]
+   #cont = 0
+    for part in parts:
+        c = 'gray'
+        #if cont == 1 or cont == 3:
+        #    c = "C" + str(cont) #C1 = Laranja, C2 = Vermelho
+        #cont += 1
+        rotated_part = rotate_transformer(part, angle)
+        plot_transformer(ax, rotated_part, c) # plotar transformador rotacionado
+
+    n1 = first_tension*1.1
+    n2 = second_tension*1.1
+
+    if(n1>n2):
+        #primeiro enrolamento
+        zline = np.linspace(0.6*a, a*1.2, 100)+a*(0.75)
+        xline = np.cos(zline*3*b)
+        yline = np.sin(zline*3*b)
+        ax.plot3D((b/1.5)*xline-(a*(-1.52)), yline*(b/1.5)+(b/2), zline, color='brown')
+
+        #segundo enrolamento
+        zline = np.linspace(a, 0.5*a, 50)-a*(0.01)
+        xline = np.cos(zline*2*b)-2.5
+        yline = np.sin(zline*2*b)
+        ax.plot3D((b/1.5)*xline+(b*2.79), yline*(b/1.5)+(b/2), zline, color='brown')
+
+    elif(n2>n1): #se n2>n1
+        #primeiro enrolamento
+        zline = np.linspace(0.6*a, a*1.2, 100)-a*(0.01)
+        xline = np.cos(zline*3*b)
+        yline = np.sin(zline*3*b)
+        ax.plot3D((b/1.5)*xline-(a*(-1.52)), yline*(b/1.5)+(b/2), zline, color='brown')
+
+        #segundo enrolamento
+        zline = np.linspace(a, 0.5*a, 50)+a*(1.01)
+        xline = np.cos(zline*2*a)-2.5
+        yline = np.sin(zline*2*a)
+        ax.plot3D((b/1.5)*xline+(b*2.79), yline*(b/1.5)+(b/2), zline, color='brown')
 
     else:
-     print("mero")
+        #primeiro enrolamento
+        zline = np.linspace(a, 0.5*a, 50)+a*(1.01)
+        xline = np.cos(zline*2*a)
+        yline = np.sin(zline*2*a)
+        ax.plot3D((b/1.5)*xline-(a*(-1.52)), yline*(b/1.5)+(b/2), zline, color='brown')
+
+        #segundo enrolamento
+        zline = np.linspace(a, 0.5*a, 50)+a*(1.01)
+        xline = np.cos(zline*2*a)
+        yline = np.sin(zline*2*a)
+        ax.plot3D((b/1.5)*xline-(a*(-1.52)), yline*(b/1.5)+(b/2), zline-a, color='brown')
--- Desafio_1_e_2/primeiro_exame_escolar.py
+++ Desafio_1_e_2/primeiro_exame_escolar.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+
 from desafio_1 import initialize_parameters, first_and_second_current, \
     conductor_section, bitola,magnectic_section,core_geometric_section_1, \
     calculate_a_and_b_geometric_section, core_geometric_section, \
-    core_magnetic_section, calculate_turns_number_1, iron_core_weight, \
-    cooper_section, calculate_lm, cooper_weight, dimensions_core
+    core_magnetic_section, calculate_turns_number_1, \
+    dimensions_core, blades_qtd, blade_type
 
 if __name__ == '__main__': 
 
@@ -14,11 +17,16 @@ if __name__ == '__main__':
     try:
         if desafio == 'D1':
             print('Escreva os parâmetros do transformador:')
+            acesita_blade_espessura= 0.035
             W2 = input('A potência de saída é: ')
             V2 = input('Tensão de saída é: ')
             V1 = input('Tensão de entrada é: ')
             frequency = input('Frequência: ')
             if initialize_parameters(W2,V2,V1) != None:
+                W2 = int(W2)
+                V2 = int(V2)
+                V1 = int(V1)
+                frequency = int(frequency)
                 first_current, second_current = first_and_second_current(W2,V2,V1)
             else:
                 print("Valores não inicializados de forma correta, assinalando valor default para zero")
@@ -31,27 +39,35 @@ if __name__ == '__main__':
             section_1_bitola = bitola(section_1)
             section_2_bitola = bitola(section_2)
 
-            magnetic_section = magnectic_section(W1,frequency,True)
+            magnetic_section = magnectic_section(W1,frequency,False)
             geometric_section = core_geometric_section_1(magnetic_section)
 
-            if geometric_section > 25: 
-                a = 5
-            else: 
-                a = 4
+            if(W2>800):
+                print("Uso das laminas Compridas pois a potencia é maior que 800VA")
+            else:
+                print("Uso das laminas padronizadas pois a potencia é menor que 800VA")
+
+            a = round(math.sqrt(geometric_section))
+            blade = blade_type(a)
+            b = round(calculate_a_and_b_geometric_section(geometric_section,a),1)
             
-            b = round(calculate_a_and_b_geometric_section(geometric_section),1)
+            
             core_gs = core_geometric_section(a, b)
             core_ms =round(core_magnetic_section(a, b),1)
             n1 = calculate_turns_number_1(frequency, W1, core_ms)
             n2 = calculate_turns_number_1(frequency,  W2, core_ms)*1.1
 
-            iron_weight = iron_core_weight(core_gs)
-            section_cooper= cooper_section(n1,section_1,n2,section_2)
-            comprimento_medio_espiras_cobre = calculate_lm(core_gs)
-            weight_cooper = cooper_weight(section_cooper,comprimento_medio_espiras_cobre)
-
             dimensions= dimensions_core(a,b,W2)
             qtd_blades= blades_qtd(b,acesita_blade_espessura)
+
+
+            print("Número de Espiras do Enrolamento Primário: ", n1)
+            print("Número de Espiras do Enrolamento Secundário: ", n2)
+            print("Bitola do cabo primário: ", section_1_bitola)
+            print("Bitola do cabo secundário: ", section_2_bitola)
+            print("Tipo de lâmina: ", blade)
+            print("Quantidade de lâminas: ", qtd_blades)
+            print("Dimensões: ",dimensions)
 
     
     except ValueError:
